c4034929f15a90fd0c23157da3ebc64046fbcd30
angel_ivanov <angelspsv@gmail.com>
2023-09-27 18:46:49 +0200

    fitxer practica1B creat

---
+++ fitxer_practica1b_angel.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php 
+        $persona = ["nom" => "Roger", "anys" => 40, "naixement" => "1983-05-12", "telefon" => 776549987];
+    ?>
+    <form>
+        <div>
+            
+
+            <p>Texto</p>
+
+            
+        </div>
+    </form>
+</body>
+</html>
