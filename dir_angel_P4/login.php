b4c3036594d9f599a4c36d43a597f2d4ace9f427
angel_ivanov <angelspsv@gmail.com>
2023-10-20 01:18:18 +0200

    feta resposta si alumne o professor, pero sin prova

--- dir_angel_P4/login.php
+++ dir_angel_P4/login.php
@@ -1,11 +1,3 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
     <?php
         //conexió a la BBDD
         include("connect_file.php");
@@ -14,14 +6,30 @@
             $email = $_POST['email'];
             $password = $_POST['password'];
 
-            echo $email;
             $select_query = "SELECT * FROM `users` WHERE `correu` = '$email' AND `contrasenya` = '$password'";
             $resultat_query = mysqli_query($conn, $select_query);
 
             if($resultat_query && mysqli_num_rows($resultat_query)>0){
+                $query_where = "SELECT rol FROM `users` WHERE correu='$email'";
+                $resultat_where = mysqli_query($conn, $query_where);
+                
                 //mostrar dades de professor o alumne
-
-
+                if ($resultat_where == "alumnat") {
+                    echo "soc un alumne<br>";
+                    echo "nom: "  .    "<br>";
+                    echo "cognom: " .   "<br>";
+                    echo "email: " . $email . "<br>";
+                } else if ($resultat_where == "profesorat") {
+                    $select_name = "SELECT nom FROM `users` WHERE correu = $email";
+                    $resultat_name = mysqli_query($conn, $select_name);
+                    $query_select_all = "SELECT nom, cognom FROM `users`";
+                    $resultat_select_all = mysqli_query($conn, $query_select_all);
+                    echo "Hola " . $resultat_name . ", ets professor!!<br>";
+                    echo "<br>";
+                    foreach ($resultat_select_all as $row) {
+                        echo "nom i cognom: " . $row;
+                    }
+                }
             } else {
                 header("Location: inici_sessio.html");
                 exit();
@@ -29,5 +37,3 @@
 
         }
     ?>
-</body>
-</html>
