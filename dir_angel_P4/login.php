68b2c7958f372e42abb61dd57c689f7df6de4970
angel_ivanov <angelspsv@gmail.com>
2023-10-20 13:41:54 +0200

    fent modificacions en l'apartat alum-prof

--- dir_angel_P4/login.php
+++ dir_angel_P4/login.php
@@ -1,6 +1,6 @@
-    <?php
-        //conexió a la BBDD
-        include("connect_file.php");
+<?php
+    //conexió a la BBDD
+    include("connect_file.php");
 
         if(isset($_POST['enter'])){
             $email = $_POST['email'];
@@ -10,30 +10,40 @@
             $resultat_query = mysqli_query($conn, $select_query);
 
             if($resultat_query && mysqli_num_rows($resultat_query)>0){
-                $query_where = "SELECT rol FROM `users` WHERE correu='$email'";
-                $resultat_where = mysqli_query($conn, $query_where);
-                
-                //mostrar dades de professor o alumne
-                if ($resultat_where == "alumnat") {
+                //l'usuari existeix
+                $row = mysqli_fetch_assoc($resultat_where);
+            $rol = $row['rol'];
+
+            if ($rol == "alumnat") {
+                // Usuario es un alumno
+                $select_name = "SELECT nom, cognom FROM `users` WHERE `correu` = '$email'";
+                $resultat_name = mysqli_query($conn, $select_name);
+
+                if ($resultat_name && mysqli_num_rows($resultat_name) > 0) {
+                    //per alumne
+                    $row = mysqli_fetch_assoc($resultat_name);
+                    $nom = $row['nom'];
+                    $cognom = $row['cognom'];
                     echo "soc un alumne<br>";
-                    echo "nom: "  .    "<br>";
-                    echo "cognom: " .   "<br>";
+                    echo "nom: ". $nom ."<br>";
+                    echo "cognom: " . $cognom . "<br>";
                     echo "email: " . $email . "<br>";
+                }
                 } else if ($resultat_where == "profesorat") {
-                    $select_name = "SELECT nom FROM `users` WHERE correu = $email";
-                    $resultat_name = mysqli_query($conn, $select_name);
+                    //per professor
                     $query_select_all = "SELECT nom, cognom FROM `users`";
                     $resultat_select_all = mysqli_query($conn, $query_select_all);
                     echo "Hola " . $resultat_name . ", ets professor!!<br>";
                     echo "<br>";
                     foreach ($resultat_select_all as $row) {
-                        echo "nom i cognom: " . $row;
+                        echo "nom i cognom: " . $row['nom'] . " " . $row['cognom'] . "<br>";
                     }
                 }
             } else {
+                //les credencials no son correctes
                 header("Location: inici_sessio.html");
                 exit();
             }
 
         }
-    ?>
+?>
