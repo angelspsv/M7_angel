d84154e0359695c8e794a0500d6afe7165e52e92
angel_ivanov <angelspsv@gmail.com>
2023-10-27 16:59:32 +0200

    creant usuari catala

--- dir_angel_P5/idioma.php
+++ dir_angel_P5/idioma.php
@@ -10,11 +10,11 @@
         //codi per mostrar l'idioma triat
         if(isset($_COOKIE['sel_idioma'])){
             if ($_COOKIE['sel_idioma'] == 'es') {
-                header('Location: spanish.html');
+                header('Location: login_spanish.html');
             } elseif ($_COOKIE['sel_idioma'] == 'en') {
-                header('Location: english.html');
+                header('Location: login_english.html');
             } elseif ($_COOKIE['sel_idioma'] == 'cat') {
-                header('Location: catala.html');
+                header('Location: login_catalan.html');
             }
         }
     ?>
