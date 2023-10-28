e5cf3f9d8ab3ad4a21e06b85cfee88c596072b63
angel_ivanov <angelspsv@gmail.com>
2023-10-28 12:26:17 +0200

    modificat temps de cookie a 60s

--- dir_angel_P5/crear_cookie.php
+++ dir_angel_P5/crear_cookie.php
@@ -1,7 +1,7 @@
 <?php
 //creació de la cookie
     echo "<h3>Creo la cookie</h3>";
-    setcookie("sel_idioma", $_GET['idioma'], time()+120);
+    setcookie("sel_idioma", $_GET['idioma'], time()+60);
 
     //retorna a menu idioma
     header('Location: idioma.php');
