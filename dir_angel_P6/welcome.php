24a7ab0b35566c7010c9cc71e2fa0bce84c22c51
angel_ivanov <angelspsv@gmail.com>
2023-11-04 09:57:13 +0100

    ajustando el archivo a las necesidades práctica

--- dir_angel_P6/welcome.php
+++ dir_angel_P6/welcome.php
@@ -4,14 +4,29 @@
     //recuperem les dades des de session
     $nom = $_SESSION['nom'];
     $cognom = $_SESSION['cognom'];
-    //$rol = $_SESSION['rol'];
-    //$contrasenya = $_SESSION['password'];
-    //$correu = $_SESSION['email'];
-    //$actiu = $_SESSION['active'];
+    $rol = $_SESSION['rol'];
+    $contrasenya = $_SESSION['contrasenya'];
+    $correu = $_SESSION['correu'];
+    
 
-    //mostrem les dades de l'usuari
-    echo "Benvingut/a,<br>";
-    echo "Hola, $nom $cognom!<br>";
-    //echo "Tu contraseña es: $contrasenya<br>";
+    if (empty($_SESSION)) {
+        echo "Sesión no iniciada.<br>";
+        $sessionID = session_id();
+        echo "El identificador de sesión es: $sessionID<br>";
+    } else {
+        echo "Sesión iniciada.</p>";
+    }
 
+    if($rol == "alumnat"){
+        //mostrem les dades alumne
+        echo "Benvingut/da, $nom $cognom!<br>";
+        echo "Les teves dades són:<br>";
+        echo "rol: $rol<br>";
+        echo "correu: $correu<br>";
+        echo "contrasenya: $contrasenya<br>";
+    } else if($rol == "professorat"){
+        //mostrem dades de professorat
+        echo "Benvingut/da $rol, $nom $cognom!<br>";
+    }
+    echo '<br><a href="delete_session.php">Destrueix la sessió</a>';
 ?>
