a1c89102c32873ff392f9d8c29d41e25f776ffb9
angel_ivanov <angelspsv@gmail.com>
2023-11-04 09:58:21 +0100

    petits canvis

--- dir_angel_P6/delete_session.php
+++ dir_angel_P6/delete_session.php
@@ -9,18 +9,11 @@
     <title>Document</title>
 </head>
 <body>
-    <span>Destruimos la sesión.</span>
+
     <?php
+    //Destruimos la sesión
         session_unset();
         session_destroy();
-
-        if (empty($_SESSION)) {
-            echo "<p>La sesión se ha eliminado con éxito.</p>";
-            $sessionID = session_id();
-            echo "El identificador de sesión es: $sessionID<br>";
-        } else {
-            echo "<p>La sesión no se ha eliminado.</p>";
-        }
 
         header('Location: login.html');
     ?>
